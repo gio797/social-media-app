4e3056e54528d2cc4a0fa392f906b3f4a1439988
gio797 <119562363+gio797@users.noreply.github.com>
2023-08-04 17:37:29 +0400

    modify posts requests

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./posts.scss";
 import Post from "../post/Post";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { makeRequest } from "../../axios.js";
 
 function Posts() {
-  const posts = [
-    {
-      id: 1,
-      name: "John Doe",
-      userId: 1,
-      profilePic:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-    {
-      id: 2,
-      name: "Jane Doe",
-      userId: 2,
-      profilePic:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
-    },
-  ];
+  // const [posts, setPosts] = useState([]);
+
+  // axios
+  //   .get("http://localhost:8800/api/posts")
+  //   .then((res) => setPosts(res.data));
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await axios.get(`http://localhost:8800/api/posts/`);
+  //       setPosts(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // useEffect(() => {
+  //   makeRequest.get("/posts").then((res) => setPosts(res.data));
+  // }, []);
+
+  const { isLoading, error, data } = useQuery("posts", () =>
+    makeRequest.get("/posts").then((res) => {
+      return res.data;
+    })
+  );
 
   return (
     <div className="posts">
-      {posts.map((post) => (
-        <Post post={post} key={post.id} />
-      ))}
+      {error
+        ? "Something went wrong"
+        : isLoading
+        ? "Loading"
+        : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
 }
